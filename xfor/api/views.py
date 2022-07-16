cbc0f03198e548e35dfcc56de99f86033c8bf9ca
LUBASKIN_CODE <lubaskincorporation@gmail.com>
2022-07-16 10:12:54 +0300

    Remove comment in api/views.py

--- xfor/api/views.py
+++ xfor/api/views.py
@@ -12,19 +12,3 @@ class CommentAddLikeAPIView(AddLikeAPIView):
 class AddLentaCommentAPIView(LentaCommentAPIView):
     comment_model = Comment
     post_model = Post
-
-# TODO: make AddLentaCommentAPIView
-# @ajax_required
-# @csrf_protect
-# def add_lenta_comment(request):
-#     response = {}
-#     if request.POST.get('post_id') and request.POST.get('body') and request.user.is_authenticated:
-#         post = get_object_or_404(Post,pk=request.POST.get('post_id'))
-#         try:
-#             Comment.objects.create(post_id=post.id,author_id=request.user.id,body=str(request.POST.get('body')))
-#         except:
-#             response['error'] = 'Неправильный формат комментария'
-#         response['status'] = 'success'
-#     else:
-#         response['error'] = 'Неправильный формат данных'
-#     return JsonResponse(response)
