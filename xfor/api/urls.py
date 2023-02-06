afd3ad981312eb8210ad7bb581f0a8e818502c75
lubaskinc0de <lubaskincorporation@gmail.com>
2023-02-06 20:18:28 +0300

    Refactoring:
    Backend :: remove 'api' app
    
    Frontend :: renamed authentication components dirs, move components of feed to the dirs

--- xfor/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PostAddLikeAPIView, CommentAddLikeAPIView
-
-urlpatterns = [
-    path("add-like/", PostAddLikeAPIView.as_view(), name="like"),
-    path("add-like-comment/", CommentAddLikeAPIView.as_view(), name="like_comment"),
-]
